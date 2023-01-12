aa2b0df64be4bfd8c72608a6bdfa92407a7fa4ef
alex-yas <alex.yas001@gmail.com>
2023-01-12 23:58:08 +0300

    Adding comments

--- Huffman.hs
+++ Huffman.hs
@@ -3,10 +3,11 @@ import Data.List
 import qualified Data.Map.Strict as Map
 import Data.Maybe
 import Data.Binary
-import Data.Binary.Builder
 import qualified Data.ByteString.Lazy as BIN
 import System.IO
+import Data.Int
 
+-- Структура для хранения узла дерева
 data Node = Node {frequency:: Float, symbol:: String, left:: Maybe Node, right:: Maybe Node}
 
 instance Eq Node where
@@ -24,18 +25,28 @@ instance Show Node where
 myNode :: Float -> String -> Maybe Node -> Maybe Node -> Node
 myNode f s l r = Node f s l r
 
+-- Создание родительского узла для двух листьев
+
 mergeLeaves :: Node -> Node -> Node
 mergeLeaves left right = myNode ((frequency left) + (frequency right)) "" (Just left) (Just right)
+
+-- Вставка нового узла с сохранением отсортированности
 
 insertNode :: Node -> [Node] -> [Node]
 insertNode x [] = [x] 
 insertNode x (y:ys) = if x <= y then x:y:ys else y : insertNode x ys
 
+-- Одна итерация построения дерева
+
 buildTree :: [Node] -> [Node]
 buildTree symbols = (drop 2 (insertNode (mergeLeaves (symbols!!0) (symbols!!1)) symbols))
 
+-- Построение завершено, когда останется лишь один лист
+
 checkBuilded :: [Node] -> Bool
 checkBuilded nodes = (length nodes == 1)
+
+-- Рекурсивный обход дерева для формирования кодов
 
 goLeft :: Node -> Map.Map String [Bool] -> [Bool] -> Map.Map String [Bool]
 goLeft root codes curCode = case (left root) of
@@ -50,33 +61,39 @@ goRight root codes curCode = case (right root) of
 calculateCodes :: Map.Map String [Bool] -> Node -> [Bool] -> Map.Map String [Bool]
 calculateCodes codes root curCode = goRight root (goLeft root codes curCode) curCode
 
+-- Разбиение массива bool на части размера 8
+
+getChunks :: Int -> [a] -> [[a]]
+getChunks _ [] = []
+getChunks n l = (take n l) : (getChunks n (drop n l))
+
+-- Формирование однобайтового числа из массива Bool
+
+convertInt8 :: [Bool] -> Int8
+convertInt8 [] = 0
+convertInt8 (x : xs) = if x then 1 + 2 * convertInt8 xs else 2 * convertInt8 xs
+
 main = do
  args <- getArgs
  let inFile = head args
  let outFile = args!!1
+
+-- Нахождение частот всех символов
  content <- readFile inFile
  let unique = nub content
  let totalNumber = fromIntegral (length content)
  let counts = [fromIntegral ((length . filter (== c)) content) / totalNumber | c <- unique]
  let symbols = [myNode f [s] Nothing Nothing | (f, s) <- (zip counts unique)]
- putStrLn (show symbols)
+
+-- Построение дерева и формирование кодов
  let root = head (until checkBuilded buildTree symbols)
  let codes = calculateCodes Map.empty root []
+
+-- Преобразование в байтовую строку
  let encoded = concat [Map.findWithDefault [] [c] codes | c <- content]
- --let compressed = putList encoded
-
-
-
- putStrLn (show encoded)
- let compressed = encode (encoded :: [Bool])
- putStrLn (show compressed)
- --let final = get compressed
- --h_out <- openFile outFile WriteMode
- --BIN.hPut h_out final
- --hClose h_out
+ let encoded_in_bytes = [convertInt8 b| b <- getChunks 8 encoded]
+ let compressed = encode (encoded_in_bytes :: [Int8])
 
  f <- openFile outFile WriteMode
  BIN.hPut f compressed
  hClose f
-
-
